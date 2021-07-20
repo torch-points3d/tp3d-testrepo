0a741e3cd283c4f627b7d797019d2c89a97c5531
Christopher Lee <csl170000@utdallas.edu>
2021-07-20 16:17:08 -0500

    implement basic dset loading

--- torch_points3d/dataset/s3dis1x1.py
+++ torch_points3d/dataset/s3dis1x1.py
@@ -3,81 +3,37 @@ from omegaconf import MISSING
 from dataclasses import dataclass
 
 import hydra.utils
-import pytorch_lightning as pl
 import torch_geometric.transforms as T
 from torch.utils.data import DataLoader
+from torch_points3d.dataset.base_dataset import PointCloudDataModule, PointCloudDataConfig
 
 from torch_geometric.datasets import S3DIS as S3DIS1x1
 
 
 @dataclass
-class S3DISDataConfig:
+class S3DISDataConfig(PointCloudDataConfig):
     batch_size: int = 32
     num_workers: int = 0
     fold: int = 6
-    dataroot: str = "data"
-    pre_transform: Sequence[Any] = None
-    train_transform: Sequence[Any] = None
-    test_transform: Sequence[Any] = None
 
 
-class s3dis_data_module(pl.LightningDataModule):
+class s3dis_data_module(PointCloudDataModule):
     def __init__(self, cfg: S3DISDataConfig = S3DISDataConfig()) -> None:
-        super().__init__()
-        self.cfg = cfg
-        self.__instantiate_transform()
+        super().__init__(cfg)
 
         self.ds = {
             "train": S3DIS1x1(
                 self.cfg.dataroot,
                 test_area=self.cfg.fold,
                 train=True,
-                pre_transform=self.pre_transform,
-                transform=self.train_transform,
+                pre_transform=self.cfg.pre_transform,
+                transform=self.cfg.train_transform,
             ),
             "test": S3DIS1x1(
                 self.cfg.dataroot,
                 test_area=self.cfg.fold,
                 train=False,
-                pre_transform=self.pre_transform,
-                transform=self.test_transform,
+                pre_transform=self.cfg.pre_transform,
+                transform=self.cfg.train_transform,
             ),
         }
-
-    def __instantiate_transform(self):
-        for transform_type in ["pre_transform", "train_transform", "test_transform"]:
-            if transform_type in self.cfg:
-                transforms = []
-                for transform in self.cfg[transform_type]:
-                    transforms.append(hydra.utils.instantiate(transform))
-                transform = T.Compose(transforms)
-                setattr(self, transform_type, transform)
-
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.ds["train"],
-            batch_size=self.batch_size,
-            num_workers=self.cfg.num_workers,
-            collate_fn=self.collate_fn,
-        )
-
-    def val_dataloader(self) -> DataLoader:
-        return DataLoader(
-            self.ds["validation"],
-            batch_size=self.batch_size,
-            num_workers=self.cfg.num_workers,
-            collate_fn=self.collate_fn,
-        )
-
-    def test_dataloader(self) -> Optional[DataLoader]:
-        if "test" in self.ds:
-            return DataLoader(
-                self.ds["test"],
-                batch_size=self.batch_size,
-                num_workers=self.cfg.num_workers,
-                collate_fn=self.collate_fn,
-            )
-
-    @property
-    def batch_size(self) -> int:
-        return self.cfg.batch_size
--- train.py
+++ train.py
@@ -1,20 +1,20 @@
 import hydra
 from hydra.core.global_hydra import GlobalHydra
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 from torch_points3d.trainer import LitTrainer
+from torch_points3d.core.instantiator import HydraInstantiator, Instantiator
 
 OmegaConf.register_new_resolver("get_filename", lambda x: x.split("/")[-1])
 
 
 @hydra.main(config_path="conf", config_name="config")
-def main(cfg):
-    OmegaConf.set_struct(
-        cfg, False
-    )  # This allows getattr and hasattr methods to function correctly
-    if cfg.pretty_print:
-        print(OmegaConf.to_yaml(cfg))
+def main(cfg: DictConfig):
+    OmegaConf.set_struct(cfg, False)  # This allows getattr and hasattr methods to function correctly
+    if cfg.get("pretty_print"):
+        print(OmegaConf.to_yaml(cfg, resolve=True))
 
-    trainer = LitTrainer(cfg)
+    instantiator = HydraInstantiator()
+    trainer = LitTrainer(instantiator, dataset=cfg.get("dataset"), trainer=cfg.get("trainer"), model=cfg.get("model"))
     trainer.train()
 
 
